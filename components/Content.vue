6ec3868baf815de8e560771adf067c2317f5c6c9
Henning Kollenbroich <hkollenbroich@tableau.com>
2022-02-03 23:59:59 +0100

    Design improvements

--- components/Content.vue
+++ components/Content.vue
@@ -17,7 +17,7 @@
         <article
           class="col-span-3 lg:grid lg:grid-cols-3 grid-flow-row-dense gap-x-16"
         >
-          <h1 class="text-4xl col-span-3">
+          <h1 class="col-span-3">
             {{ content.title }}
           </h1>
           <aside class="col-start-3 text-gray-500 text-sm">
@@ -32,24 +32,5 @@
 
 <script setup lang="ts">
 const { $supabase } = useNuxtApp();
-// const { $markdown } = useNuxtApp();s
-// const { data } = await $supabase
-//   .from("user_content")
-//   .select("title, data, type, target_url")
-//   .gt("inserted_at", new Date(Date.now() - 86400000).toISOString()) // Only past 24 hours
-//   .order("type", { ascending: false });
 const data = await $fetch("/api/get");
 </script>
-
-<style scoped>
-article h1 {
-  font-family: "Hoefler Text";
-  font-style: italic;
-  /* text-transform: capitalize; */
-}
-
-article aside {
-  /* margin-top: 1rem; */
-  margin-bottom: 4rem;
-}
-</style>
