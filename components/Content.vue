bf9f432893f505703557817e4e45bc52f9e2e4f0
Henning Kollenbroich <hkollenbroich@tableau.com>
2022-02-07 22:37:18 +0100

    fixed styling, spacing

--- components/Content.vue
+++ components/Content.vue
@@ -2,16 +2,16 @@
   <ul class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-x-16">
     <template v-for="(content, index) in data" :key="index">
       <li
-        class="border-t pt-16 pb-16 border-t-gray-30"
+        class="border-t pt-8 pb-8 border-t-gray-30"
         v-if="content.type === 'blurb'"
       >
         <section>
-          <h1 class="text-4xl">{{ content.title }}</h1>
+          <h1>{{ content.title }}</h1>
           <span v-html="content.data"></span>
         </section>
       </li>
       <li
-        class="col-span-2 lg:col-span-3 border-t pt-16 border-t-gray-300"
+        class="col-span-2 lg:col-span-3 border-t pt-8 pb-8 border-t-gray-300"
         v-if="content.type === 'article'"
       >
         <article
@@ -20,7 +20,7 @@
           <h1 class="col-span-3">
             {{ content.title }}
           </h1>
-          <aside class="col-start-3 text-gray-500 text-sm">
+          <aside class="col-start-3 text-gray-500">
             <a :href="content.target_url">{{ content.target_url }}</a>
           </aside>
           <main class="col-span-2" v-html="content.data"></main>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="w-full min-h-screen bg-white dark:bg-black dark:text-white p-8">
     <div v-if="isAuthenticated">
       <Logout />
-      <h1 class="text-7xl font-bold mb-8">Brief.</h1>
+      <h1>Brief.</h1>
       <Content />
     </div>
     <Auth v-else />
