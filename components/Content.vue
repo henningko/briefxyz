74a84c4e2108d14662ef6064442a7bb9ef4bdb33
Henning Kollenbroich <hkollenbroich@tableau.com>
2022-02-11 14:04:31 +0100

    marking text first version

--- components/Content.vue
+++ components/Content.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+    <p v-for="highlight in highlights">{{ highlight.text }}</p>
+  </div>
   <ul class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-x-16">
     <template v-for="(content, index) in data" :key="index">
       <li
@@ -16,6 +19,7 @@
       >
         <article
           class="col-span-3 lg:grid lg:grid-cols-3 grid-flow-row-dense gap-x-16"
+          @mouseup="mark"
         >
           <h1 class="col-span-3">
             {{ content.title }}
@@ -33,4 +37,41 @@
 <script setup lang="ts">
 const { $supabase } = useNuxtApp();
 const data = await $fetch("/api/get");
+
+let highlights = ref([]);
+
+const mark = (event) => {
+  const selection = document.getSelection();
+  // Only do this if it's not an empty selection
+  if (!selection.isCollapsed) {
+    const range = selection.getRangeAt(0);
+    let isValidSelection = false;
+    console.log(range);
+    let startNode = range.startContainer.parentNode;
+    let endNode = range.endContainer.parentNode;
+    // Check if invalid selection can be made valid through expansion
+    if (startNode != endNode) {
+      if (startNode == endNode.parentNode) {
+        range.setEndAfter(endNode);
+        endNode = range.endContainer.parentNode;
+        isValidSelection = true;
+      } else if (startNode.parentNode == endNode) {
+        range.setStartBefore(startNode);
+        startNode = range.startContainer.parentNode;
+        isValidSelection = true;
+      } else {
+        isValidSelection = false;
+      }
+    } else {
+      isValidSelection = true;
+    }
+    if (isValidSelection) {
+      const id = `highlight-${highlights.value.length}`;
+      let wrap = document.createElement("mark");
+      wrap.id = id;
+      range.surroundContents(wrap);
+      highlights.value.push({ id: id, text: range.toString() });
+    }
+  }
+};
 </script>
