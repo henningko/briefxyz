fdd356048757abeb68d541f20120586b13e4bc0b
Henning Kollenbroich <henning.kollenbroich@gmail.com>
2022-06-24 16:13:05 +0200

    styling improvements
    added title, fixed baseline grid alignment

--- app.vue
+++ app.vue
@@ -8,4 +8,9 @@
 <script setup lang="ts">
 import "./assets/css/normalize.css";
 import "./assets/css/global.css";
+
+useHead({
+  title: "Brief",
+  viewport: "width=device-width, initial-scale=1",
+});
 </script>
